4fc90dd4bbc734450d4883ba2a86984fe58e99a9
arunstop <arunstop@gmail.com>
2023-01-17 20:32:53 +0700

    chore : more tailwind/next/prettier configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,38 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.tsx", "./ui/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "sofia-sans": ["Sofia Sans"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require("daisyui"),
+    require("tailwindcss/plugin")(function ({ addVariant }) {
+      addVariant("hocus", ["&:hover", "&:focus"])
+    }),
+  ],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=corporate]"],
+          "--btn-text-case": "none",
+          "--rounded-box": "0.25rem",
+          "--rounded-btn": "0.5rem",
+        },
+      },
+      {
+        dark: {
+          ...require("daisyui/src/colors/themes")["[data-theme=business]"],
+          "--btn-text-case": "none",
+          "--rounded-box": "0.25rem",
+          "--rounded-btn": "0.5rem",
+        },
+      },
+    ],
+  },
 }
