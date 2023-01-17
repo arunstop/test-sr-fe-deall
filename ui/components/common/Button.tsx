62bc88ed4f7bdcdabf775c5eeac62913eca8887c
arunstop <arunstop@gmail.com>
2023-01-18 04:30:25 +0700

    style : added gap to  `Button` style

--- ui/components/common/Button.tsx
+++ ui/components/common/Button.tsx
@@ -7,7 +7,8 @@ const Button = React.forwardRef<
   return (
     <button
       ref={ref}
-      className={`btn min-w-[4rem] hover:-translate-y-2 active:!scale-90 active:translate-y-0 ${
+      className={`btn min-w-[4rem] hover:-translate-y-2 active:!scale-90 active:translate-y-0 gap-2 sm:gap-4
+      ${
         className
       }`}
       {...props}
