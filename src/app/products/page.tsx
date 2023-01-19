b3d05a3cd3cfa85548fbfc3598520624e5491c3f
arunstop <arunstop@gmail.com>
2023-01-20 03:38:28 +0700

    feat : added filter by category

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,67 +1,62 @@
 "use client"
+import { ICategory } from "core/models/category"
 import { IProduct } from "core/models/product"
-import { repoProductGet } from "core/repos/product"
+import { repoCategoryGetAll } from "core/repos/category"
+import { repoProductGetAll } from "core/repos/product"
 import { IPaging, ISearch } from "core/types/main"
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useMemo, useState } from "react"
 import Pagination from "ui/components/common/Pagination"
+import SelectInput from "ui/components/common/SelectInput"
 import TextInput from "ui/components/common/TextInput"
+import ProductFilterSection from "ui/components/product/ProductFilterSection"
 import DashboardContentLayout from "ui/layouts/DashboardContentLayout"
 
-export type IProductList = { products: IProduct[] } & IPaging&ISearch
+type IProductsPagination = IPaging & ISearch
+
+export type IProductList = { products: IProduct[] } & IProductsPagination
 
 function ProductsPage() {
-  const [products, setProducts] = useState<IProductList>()
-
-  async function getProducts(props?: Partial<IPaging & ISearch>) {
-    // if
-    const products = await repoProductGet(props)
-      .then(async (res) => (await res.json()) as IProductList)
-      .catch((err) => console.error("Error when fetching data: ", err))
-    if (!products) return
-    setProducts(products)
-  }
-
-  useEffect(() => {
-    getProducts({
-      q: "",
-      limit: 10,
-      skip: 0,
-    })
-    return () => {}
-  }, [])
-
-  const handleSearch = React.useCallback(
-    (q: string) => {
-      getProducts({
-        q: q,
-        limit: products?.limit,
-        skip: products?.skip,
-        total: products?.total,
-      })
-    },
-    [products]
-  )
-
+  const {
+    products,
+    categories,
+    category,
+    setCategory,
+    productsPagination,
+    getProducts,
+    handleSearch,
+    handleFilter,
+  } = useProductsPageHook()
   return (
     <DashboardContentLayout title="Products">
       <div className="flex flex-col gap-i">
-        <section className="self-end">
-          <form
-            onSubmit={(ev) => {
-              ev.preventDefault()
-              handleSearch((ev.target as HTMLFormElement).q.value)
-              // handleSearch(ev.target as HTMLFormElement)
-            }}
-          >
-            <TextInput
-              className=" valid:outline-primary transition-all duration-300"
-              placeholder="Search Product..."
-              type="search"
-              name="q"
-              minLength={2}
+        <div className="flex max-sm:flex-col-reverse gap-i sm:justify-between max-sm:items-end">
+            <ProductFilterSection
+            className="max-sm:justify-end"
+              category={{
+                value: category,
+                list: categories,
+                set: (newVal) =>
+                  handleFilter({ category: newVal, ...productsPagination }),
+              }}
             />
-          </form>
-        </section>
+          <section className="flex gap-2 sm:gap-4 ">
+            <form
+              onSubmit={(ev) => {
+                ev.preventDefault()
+                handleSearch((ev.target as HTMLFormElement).q.value)
+                // handleSearch(ev.target as HTMLFormElement)
+              }}
+            >
+              <TextInput
+                className=" valid:outline-primary transition-all duration-300"
+                placeholder="Search Product..."
+                type="search"
+                name="q"
+                minLength={2}
+              />
+            </form>
+          </section>
+        </div>
         <section className="flex flex-col gap-i">
           <article className="overflow-x-auto">
             <table className="table table-zebra w-full ">
@@ -110,27 +105,108 @@ function ProductsPage() {
                 limit={products.limit}
                 onPrev={() =>
                   getProducts({
-                    q: products.q,
-                    limit: products.limit,
+                    ...productsPagination,
                     skip: products.skip - products.limit,
-                    total: products.total,
                   })
                 }
                 onNext={() =>
                   getProducts({
-                    q: products.q,
-                    limit: products.limit,
+                    ...productsPagination,
                     skip: products.skip + products.limit,
-                    total: products.total,
                   })
                 }
-              ></Pagination>
+              />
             </footer>
           )}
         </section>
       </div>
     </DashboardContentLayout>
   )
+}
+
+function useProductsPageHook() {
+  const [products, setProducts] = useState<IProductList>()
+  const [categories, setCategories] = useState<ICategory[]>([])
+  const [category, setCategory] = useState("")
+
+  const productsPagination: IProductsPagination = useMemo(
+    () => ({
+      q: products?.q || "",
+      limit: products?.limit || 0,
+      skip: products?.skip || 0,
+      total: products?.total || 0,
+    }),
+    [products]
+  )
+
+  async function getCategories() {
+    const newCategories = await repoCategoryGetAll()
+      .then(async (res) => (await res.json()) as string[])
+      .catch((err) => {
+        console.error("Error when fetching data: ", err)
+        return null
+      })
+    if (!newCategories) return
+    setCategories(newCategories.map((e) => ({ name: e } as ICategory)))
+  }
+
+  async function getProducts(
+    props?: Partial<IProductsPagination>,
+    cat?: string
+  ) {
+    // if
+    const products = await repoProductGetAll(props, cat)
+      .then(async (res) => (await res.json()) as IProductList)
+      .catch((err) => {
+        console.error("Error when fetching data: ", err)
+        return null
+      })
+    if (!products) return
+    setProducts(products)
+  }
+
+  const handleSearch = React.useCallback(
+    (q: string) => {
+      getProducts({
+        ...productsPagination,
+        q: q,
+      })
+    },
+    [productsPagination]
+  )
+  const handleFilter = React.useCallback(
+    ({
+      category: newCategory,
+      ...filter
+    }: { category: string } & IProductsPagination) => {
+      if (newCategory === category) return
+      setCategory(newCategory)
+      getProducts({ ...filter }, newCategory)
+    },
+    [category]
+  )
+
+  // init
+  useEffect(() => {
+    getProducts({
+      q: "",
+      limit: 10,
+      skip: 0,
+    })
+    getCategories()
+    return () => {}
+  }, [])
+
+  return {
+    products,
+    categories,
+    productsPagination,
+    category,
+    setCategory,
+    getProducts,
+    handleSearch,
+    handleFilter,
+  }
 }
 
 export default ProductsPage
