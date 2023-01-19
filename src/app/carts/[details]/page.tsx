23002ceabed1db2c7a08ec1a05437af484211d35
arunstop <arunstop@gmail.com>
2023-01-19 23:08:54 +0700

    refactor : applying the new repos on carts detail page

--- src/app/carts/[details]/page.tsx
+++ src/app/carts/[details]/page.tsx
@@ -1,72 +1,13 @@
 "use client"
+import { ICart } from "core/models/cart"
 import { IProduct } from "core/models/product"
+import { IUser } from "core/models/user"
+import { repoCartGetOne } from "core/repos/cart"
+import { repoProductGetOne } from "core/repos/product"
+import { repoUserGetOne } from "core/repos/user"
 import { useEffect, useState } from "react"
 import Pagination from "ui/components/common/Pagination"
 import DashboardContentLayout from "ui/layouts/DashboardContentLayout"
-import { ICart } from "../page"
-
-export interface IUser {
-  id: number
-  firstName: string
-  lastName: string
-  maidenName: string
-  age: number
-  gender: string
-  email: string
-  phone: string
-  username: string
-  password: string
-  birthDate: Date
-  image: string
-  bloodGroup: string
-  height: number
-  weight: number
-  eyeColor: string
-  hair: IUserHair
-  domain: string
-  ip: string
-  address: IUserAddress
-  macAddress: string
-  university: string
-  bank: IUserBank
-  company: IUserCompany
-  ein: string
-  ssn: string
-  userAgent: string
-}
-
-export interface IUserAddress {
-  address: string
-  city: string
-  coordinates: IUserCoordinates
-  postalCode: string
-  state: string
-}
-
-export interface IUserCoordinates {
-  lat: number
-  lng: number
-}
-
-export interface IUserBank {
-  cardExpire: string
-  cardNumber: string
-  cardType: string
-  currency: string
-  iban: string
-}
-
-export interface IUserCompany {
-  address: IUserAddress
-  department: string
-  name: string
-  title: string
-}
-
-export interface IUserHair {
-  color: string
-  type: string
-}
 
 function CartsDetailsPage({
   params,
@@ -82,15 +23,13 @@ function CartsDetailsPage({
 
   async function getCartDetails() {
     try {
-      const cart = await fetch(`https://dummyjson.com/carts/${details}`, {
-        method: "GET",
-      })
-        .then(async (res) => (await res.json()) as ICart)
+      const cart = await repoCartGetOne(details)
+        ?.then(async (res) => (await res.json()) as ICart)
         .catch((err) => console.error("Error when fetching data: ", err))
       if (!cart) return
-      const userPromise = getUser(cart.userId).then((user) => user.json())
+      const userPromise = repoUserGetOne(cart.userId).then((user) => user.json())
       const productsPromise = cart.products.map((product) =>
-        getProduct(product.id).then((e) => e.json())
+        repoProductGetOne(product.id).then((e) => e.json())
       )
 
       const newUser = (await userPromise) as IUser
@@ -113,17 +52,9 @@ function CartsDetailsPage({
     }
   }
 
-  async function getUser(userId: number) {
-    return fetch(`https://dummyjson.com/users/${userId}`, {
-      method: "GET",
-    })
-  }
+  
 
-  async function getProduct(productId: number) {
-    return fetch(`https://dummyjson.com/products/${productId}`, {
-      method: "GET",
-    })
-  }
+  
 
   useEffect(() => {
     getCartDetails()
@@ -197,8 +128,8 @@ function CartsDetailsPage({
                     <td className="group-hover:text-primary group-hover:bg-primary/30">
                       {e.stock}
                     </td>
-                     <td className="group-hover:text-primary group-hover:bg-primary/30 capitalize">
-                      {e.category.replaceAll("-"," ")}
+                    <td className="group-hover:text-primary group-hover:bg-primary/30 capitalize">
+                      {e.category.replaceAll("-", " ")}
                     </td>
                   </tr>
                 ))}
@@ -206,7 +137,7 @@ function CartsDetailsPage({
             </table>
           </article>
           <footer className="self-end">
-            <Pagination limit={100} skip={10} per={10}></Pagination>
+            <Pagination total={100} skip={10} limit={10}></Pagination>
           </footer>
         </section>
       </div>
