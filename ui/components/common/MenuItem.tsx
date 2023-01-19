afc37793d6cba0002084ec52874392abe8881a8e
arunstop <arunstop@gmail.com>
2023-01-20 05:40:39 +0700

    style : added icon to menu items

--- ui/components/common/MenuItem.tsx
+++ ui/components/common/MenuItem.tsx
@@ -3,12 +3,17 @@ import React from "react"
 const MenuItem = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement> & { active?: boolean }
->(({ children, className, active, ...props }, ref) => (
+>(({ children, className = "", active, ...props }, ref) => (
   <div
     ref={ref}
     className={`px-2  sm:px-4 py-1.5 sm:py-3  hover:bg-base-300/50 transition-all rounded-md sm:rounded-lg
-    active:text-base-content active:scale-90 text-base sm:text-lg font-bold cursor-pointer
-    ${active ? "border-l-2 sm:border-l-4 border-primary-focus bg-primary-focus/10 !rounded-l-none" : ""}
+    active:text-base-content active:scale-90 text-base sm:text-lg font-bold cursor-pointer items-center [&>*]:flex 
+    flex gap-2 sm:gap-4
+    ${
+      active
+        ? "border-l-2 sm:border-l-4 border-primary-focus !bg-primary-focus/10 !rounded-l-none text-primary-focus"
+        : ""
+    }
     ${className}`}
     {...props}
   >
--- ui/components/main/SidebarMain.tsx
+++ ui/components/main/SidebarMain.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { Icon } from "@iconify-icon/react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import MenuItem from "../common/MenuItem"
@@ -8,10 +9,16 @@ function SidebarMain() {
   return (
     <nav className="p-2 sm:p-4 w-80 bg-base-200  flex-col hidden lg:flex">
       <Link href="/products" >
-        <MenuItem active={path.includes("/products") == true}>Products</MenuItem>
+        <MenuItem active={path.includes("/products") == true}>
+        <Icon icon="mdi:package-variant-closed"  className="text-xl sm:tex-2xl"/>
+        <span>Products</span>
+        </MenuItem>
       </Link>
       <Link href="/carts" >
-        <MenuItem active={path.includes("/carts") == true}>Carts</MenuItem>
+        <MenuItem active={path.includes("/carts") == true}>
+        <Icon icon="mdi:cart"  className="text-xl sm:tex-2xl"/>
+        <span>Carts</span>
+        </MenuItem>
       </Link>
     </nav>
   )
