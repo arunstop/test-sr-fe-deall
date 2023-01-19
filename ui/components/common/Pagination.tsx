edcfcfe40d963a582d82cfc8d3db17703a894430
arunstop <arunstop@gmail.com>
2023-01-19 22:04:42 +0700

    feat : added pagination prev and next func

--- ui/components/common/Pagination.tsx
+++ ui/components/common/Pagination.tsx
@@ -4,23 +4,38 @@ import Button from "./Button"
 const Pagination = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLElement> & {
-    limit: number
+    total: number
     skip: number
-    per: number
+    limit: number
+    onPrev?: () => void
+    onNext?: () => void
   }
->(({ limit, skip, per, children, className = "", ...props }, ref) => {
-  const first = skip < per
-  const last = limit-skip  <= per
-  return (
-    <aside
-      className={`flex gap-2 sm:gap-4 items-center text-lg ${className}`}
-      {...props}
-    >
-      {!first && <Button className="">Prev</Button>}
-      {`Page ${skip} / ${limit}`}
-      {!last && <Button className="">Next</Button>}
-    </aside>
-  )
-})
+>(
+  (
+    { total, skip, limit, onPrev, onNext, children, className = "", ...props },
+    ref
+  ) => {
+    const first = skip < limit
+    const last = total - skip <= limit
+    return (
+      <aside
+        className={`flex gap-2 sm:gap-4 items-center text-lg ${className}`}
+        {...props}
+      >
+        {!first && (
+          <Button className="" onClick={onPrev}>
+            Prev
+          </Button>
+        )}
+        {`Page ${Math.round(skip / limit+ 1) } / ${Math.round(total / limit)}`}
+        {!last && (
+          <Button className="" onClick={onNext}>
+            Next
+          </Button>
+        )}
+      </aside>
+    )
+  }
+)
 
 export default Pagination
